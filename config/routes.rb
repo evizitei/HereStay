45485d7c7038191189dffdad774790a2d8c234d8
Ethan Vizitei <ethan.vizitei@gmail.com>
2010-10-20 11:21:18 -0500

    start using better routes (THANK YOU IFRAME)!
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -21,33 +21,39 @@ Micasasucasa::Application.routes.draw do |map|
   match "/video_uploaded"=>"my_rental_units#video_uploaded"
   
   match "/debug/geocode"=>"debug#geocode"
-  
-  scope "canvas" do 
-    match "/search"=>"canvas#search"
-    match "/my_rental_units"=>"my_rental_units#index"
-    match "/my_rental_units/show"=>"my_rental_units#show"
-    match "/manage_my_rental_units"=>"my_rental_units#manage"
-    match "/my_rental_units/new"=>"my_rental_units#new"
-    match "/my_rental_units/edit"=>"my_rental_units#edit"
-    match "my_rental_listings/save"=>"my_rental_units#save"
-    match "/my_rental_units/share"=>"my_rental_units#share"
-    match "/my_rental_unit_photos"=>"my_rental_units#photos"
-    match "/my_rental_units/delete_photo"=>"my_rental_units#delete_photo"
-    match "/my_rental_listings/new_photo"=>"my_rental_units#new_photo"
-    match "/my_rental_listings/owned_by"=>"my_rental_units#owned_by"
-    match "/my_rental_unit_video"=>"my_rental_units#upload_video"
-    match "/my_rental_units/delete"=>"my_rental_units#delete"
-    match "/my_rental_units/bookings"=>"bookings#index"
-    match "/my_rental_units/new_booking"=>"bookings#new"
-    match "/my_rental_units/create_booking"=>"bookings#create"
-    match "/bookings/discuss"=>"bookings#discuss"
-    match "/booking/messages/create"=>"bookings#create_message"
-    match "/booking/confirm"=>"bookings#confirm"
-    match "/booking/details"=>"bookings#details"
-    match "/bookings/wall_post"=>"bookings#wall_post"
-    match "/bookings/exec_confirm"=>"bookings#exec_confirm"
-    match "/account/edit"=>"account#edit"
-    match "/account/save"=>"account#save"
+
+  resources :my_rental_units do
+    collection do
+      get :manage
+    end
   end
+  
+  # scope "canvas" do 
+  #   match "/search"=>"canvas#search"
+  #   match "/my_rental_units"=>"my_rental_units#index"
+  #   match "/my_rental_units/show"=>"my_rental_units#show"
+  #   match "/manage_my_rental_units"=>"my_rental_units#manage"
+  #   match "/my_rental_units/new"=>"my_rental_units#new"
+  #   match "/my_rental_units/edit"=>"my_rental_units#edit"
+  #   match "my_rental_listings/save"=>"my_rental_units#save"
+  #   match "/my_rental_units/share"=>"my_rental_units#share"
+  #   match "/my_rental_unit_photos"=>"my_rental_units#photos"
+  #   match "/my_rental_units/delete_photo"=>"my_rental_units#delete_photo"
+  #   match "/my_rental_listings/new_photo"=>"my_rental_units#new_photo"
+  #   match "/my_rental_listings/owned_by"=>"my_rental_units#owned_by"
+  #   match "/my_rental_unit_video"=>"my_rental_units#upload_video"
+  #   match "/my_rental_units/delete"=>"my_rental_units#delete"
+  #   match "/my_rental_units/bookings"=>"bookings#index"
+  #   match "/my_rental_units/new_booking"=>"bookings#new"
+  #   match "/my_rental_units/create_booking"=>"bookings#create"
+  #   match "/bookings/discuss"=>"bookings#discuss"
+  #   match "/booking/messages/create"=>"bookings#create_message"
+  #   match "/booking/confirm"=>"bookings#confirm"
+  #   match "/booking/details"=>"bookings#details"
+  #   match "/bookings/wall_post"=>"bookings#wall_post"
+  #   match "/bookings/exec_confirm"=>"bookings#exec_confirm"
+  #   match "/account/edit"=>"account#edit"
+  #   match "/account/save"=>"account#save"
+  # end
 
 end
