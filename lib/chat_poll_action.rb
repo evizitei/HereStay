b1bb7dfbc32d7ba91ae6dd742203f988163aada3
evizitei <ethan.vizitei@gmail.com>
2010-11-06 10:09:43 -0500

    incremental fix

--- lib/chat_poll_action.rb
+++ lib/chat_poll_action.rb
@@ -3,20 +3,20 @@ class ChatPollAction < Cramp::Action
   on_start :retrieve_chats
   
   def load_booking
-    @booking = Booking.find(params[:booking])
+    #@booking = Booking.find(params[:booking])
   end
   
   def load_user
-    @user = User.find_by_fb_user_id(params[:user])
+    #@user = User.find_by_fb_user_id(params[:user])
   end
 
   def retrieve_chats 
-    chats = @booking.booking_messages.where(["id > ?",params[:last_message]])
-    list = chats.map { |msg| { "message_class" => msg.html_class, 
-                               "sent_at" => msg.created_at.to_formatted_s(:short), 
-                               "message" => msg.message, 
-                               "user_fb_id" => msg.user_fb_id } }
-    render [list.to_json,"\n"]
+    # chats = @booking.booking_messages.where(["id > ?",params[:last_message]])
+    #     list = chats.map { |msg| { "message_class" => msg.html_class, 
+    #                                "sent_at" => msg.created_at.to_formatted_s(:short), 
+    #                                "message" => msg.message, 
+    #                                "user_fb_id" => msg.user_fb_id } }
+    render "Testing"#[list.to_json,"\n"]
     finish
   end
 end
