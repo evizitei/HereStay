a8b3fb16aed25fbae8e617e9e6267f256993d391
Ethan Vizitei <ethan.vizitei@gmail.com>
2010-11-08 16:55:23 -0600

    incremental fix

--- config/initializers/cramp_server.rb
+++ config/initializers/cramp_server.rb
@@ -1,1 +1,5 @@
 Cramp::Websocket.backend = :thin
+logger = ActiveSupport::BufferedLogger.new(File.join(File.dirname(__FILE__),"..","..","log","cramp.log"))
+logger.level = ActiveSupport::BufferedLogger::DEBUG
+logger.auto_flushing = true
+Cramp.logger = logger
---
+++ spec/javascripts/ChatSpec.js
@@ -0,0 +1,2 @@
+describe("ChatterBox",function(){
+});
