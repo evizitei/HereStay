c92184f8214e1e40148bf9320b13f3ddfdb9195c
Dmitry Penkin <dmitry.penkin@gmail.com>
2010-12-14 21:35:12 +0600

    Fix migrate

--- db/migrate/20101214122714_add_accept_bids_under_minimum_to_lots.rb
+++ db/migrate/20101214122714_add_accept_bids_under_minimum_to_lots.rb
@@ -1,6 +1,6 @@
 class AddAcceptBidsUnderMinimumToLots < ActiveRecord::Migration
   def self.up
-    add_column :lots, :accept_bids_under_minimum_to_lots, :boolean, :default => 0, :null => false
+    add_column :lots, :accept_bids_under_minimum_to_lots, :boolean, :default => false, :null => false
   end
 
   def self.down
