ba83708382efe98209960be6b7f0bd1c930d145b
Dmitry Penkin <dmitry.penkin@gmail.com>
2010-12-29 18:33:20 +0600

    Use local Solr instead of Websolr

--- config/initializers/websolr.rb
+++ config/initializers/websolr.rb
@@ -1,5 +1,5 @@
 if ENV['WEBSOLR_URL']
   Sunspot.config.solr.url = ENV['WEBSOLR_URL']
-elsif File.exists?("#{Rails.root}/config/websolr.conf")
-  Sunspot.config.solr.url = File.read("#{Rails.root}/config/websolr.conf")
+# elsif File.exists?("#{Rails.root}/config/websolr.conf")
+#   Sunspot.config.solr.url = File.read("#{Rails.root}/config/websolr.conf")
 end
