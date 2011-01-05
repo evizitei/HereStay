378d6d671901e57fb24656ec94c45b3cd2df29c6
Dmitry Penkin <dmitry.penkin@gmail.com>
2011-01-05 15:43:16 +0600

    Store images on the local filesystem

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,13 +4,7 @@ require 'open-uri'
 class Photo < ActiveRecord::Base
   attr_accessor :image_url
   
-  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>", :mini => "50x50>" },
-                      :storage => :s3,:s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
-                      :path => "child/:attachment/:id/:style/:basename.:extension",
-                      :s3_host_alias => "s3.micasa-fb.com", 
-                      :bucket => "s3.micasa-fb.com", 
-                      :url=>":s3_alias_url"
-                      
+  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>", :mini => "50x50>" }
   belongs_to :rental_unit
   
   validates_presence_of :image_remote_url, :if => :image_url_provided?, :message => 'is invalid or inaccessible'
